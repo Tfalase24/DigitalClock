ea5dcfe96feda308f540e231a3724ba285bf110e
TF <tfalase24@gmail.com>
2022-10-11 17:52:27 +0100

    Added comments

--- JavaProjects/src/Main.java
+++ JavaProjects/src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String [] args){
 
-        MyFrame myframe = new MyFrame();
+        MyFrame myframe; // This is the runner class that runs an instance of the MyFrame class
+        myframe = new MyFrame();
     }
 }
--- JavaProjects/src/MyFrame.java
+++ JavaProjects/src/MyFrame.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 
 public class MyFrame extends JFrame {
 
-    Calendar calendar;
     SimpleDateFormat timeFormat; // SimpleDateFormat is a concrete class for formatting and parsing dates in a locale-sensitive manner
     SimpleDateFormat dayFormat;
     SimpleDateFormat dateFormat;
@@ -16,37 +15,37 @@ public class MyFrame extends JFrame {
     String day;
     String date;
 
-    MyFrame(){
+    MyFrame(){ // This constructor is used to create the template for each instance
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("MY DIGITAL CLOCK PROGRAM");
         this.setLayout(new FlowLayout());
         this.setSize(350, 200);
         this.setResizable(true);
 
-        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        timeFormat = new SimpleDateFormat("hh:mm:ss a"); // this concrete class sets a pattern for the information you want to display
         dayFormat = new SimpleDateFormat("E");
         dateFormat = new SimpleDateFormat("Y:M:d");
 
-        timeLabel = new JLabel();
+        timeLabel = new JLabel(); // this is a label for JFrame
 
         dayLabel = new JLabel();
 
         dateLabel = new JLabel();
 
-        this.add(timeLabel);
+        this.add(timeLabel); // this adds the label to the JFrame everytime you create an instance
         this.add(dayLabel);
         this.add(dateLabel);
 
         this.setVisible(true);
 
-        setTime();
+        setTime(); // this is a method that performs one function in the JFrame
 
     }
 
     public void setTime() {
         while (true) {
-            time = timeFormat.format(Calendar.getInstance().getTime());
-            timeLabel.setText(time);
+            time = timeFormat.format(Calendar.getInstance().getTime()); // This string gets the relevant data from the Calendar
+            timeLabel.setText(time); // and it is set here in the timeLabel
 
             day = dayFormat.format(Calendar.getInstance().getTime());
             dayLabel.setText(day);
@@ -55,7 +54,7 @@ public class MyFrame extends JFrame {
             dateLabel.setText(date);
 
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000); // This thread is to update the JFrame every second, it will through an Interruption Exception
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
